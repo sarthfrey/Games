69848e1c79606577830c3df83a9a6aa1d262decf
s-frey <sarth.frey@gmail.com>
2015-05-27 00:22:11 -0400

    added initial build for PvP
    Character and command line based tic tac toe game, supporting player
    versus player mode, built in Python.

---
+++ Tic Tac Toe/Tic Tac Toe.py
@@ -0,0 +1,94 @@
+while 0 == 0:
+    
+    first = print('1. . .')
+    second = print('2. . .')
+    third = print('3. . .')
+    fourth = print(' A B C')
+    point = input('Input an alpha-numerical specification: ')
+    if point == 'A1':
+        first = print('1X . .')
+        second = print('2. . .')
+        third = print('3. . .')
+        fourth = print(' A B C')
+        point = input()
+        if point == 'A2':
+                first = print('1. . .')
+                second = print('2X . .')
+                third = print('3. . .')
+                fourth = print(' A B C')
+        if point == 'A3':
+                first = print('1. . .')
+                second = print('2. . .')
+                third = print('3X . .')
+                fourth = print(' A B C')
+        if point == 'B1':
+                first = print('1. X .')
+                second = print('2. . .')
+                third = print('3. . .')
+                fourth = print(' A B C')
+        if point == 'B2':
+                first = print('1. . .')
+                second = print('2. X .')
+                third = print('3. . .')
+                fourth = print(' A B C')
+        if point == 'B3':
+                first = print('1. . .')
+                second = print('2. . .')
+                third = print('3. X .')
+                fourth = print(' A B C')
+        if point == 'C1':
+                first = print('1. . X')
+                second = print('2. . .')
+                third = print('3. . .')
+                fourth = print(' A B C')
+        if point == 'C2':
+                first = print('1. . .')
+                second = print('2. . X')
+                third = print('3. . .')
+                fourth = print(' A B C')
+        if point == 'C3':
+                first = print('1. . .')
+                second = print('2. . .')
+                third = print('3. . X')
+                fourth = print(' A B C')
+
+    if point == 'A2':
+        first = print('1. . .')
+        second = print('2X . .')
+        third = print('3. . .')
+        fourth = print(' A B C')
+    if point == 'A3':
+        first = print('1. . .')
+        second = print('2. . .')
+        third = print('3X . .')
+        fourth = print(' A B C')
+    if point == 'B1':
+        first = print('1. X .')
+        second = print('2. . .')
+        third = print('3. . .')
+        fourth = print(' A B C')
+    if point == 'B2':
+        first = print('1. . .')
+        second = print('2. X .')
+        third = print('3. . .')
+        fourth = print(' A B C')
+    if point == 'B3':
+        first = print('1. . .')
+        second = print('2. . .')
+        third = print('3. X .')
+        fourth = print(' A B C')
+    if point == 'C1':
+        first = print('1. . X')
+        second = print('2. . .')
+        third = print('3. . .')
+        fourth = print(' A B C')
+    if point == 'C2':
+        first = print('1. . .')
+        second = print('2. . X')
+        third = print('3. . .')
+        fourth = print(' A B C')
+    if point == 'C3':
+        first = print('1. . .')
+        second = print('2. . .')
+        third = print('3. . X')
+        fourth = print(' A B C')
